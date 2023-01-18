4e4c361ea9ecfd3cc1f4b2b50518968a49e3d932
Ante <ante.filipovic72@gmail.com>
2023-01-18 22:49:14 +0100

    a

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(
+        AuthenticationUtils $authenticationUtils,
+        Request $request
+    ): Response
+    {
+        $data = $request->getContent();
+        $data = json_decode($data, true);
+
+        dd($request->getContent());
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->json(['last_username' => $request->getContent()]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
